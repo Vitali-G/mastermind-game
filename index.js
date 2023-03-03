e53cb05aa8345a564d5aa29a4a4876e4d012595e
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-03-03 12:05:15 +0000

    clear on intro invocation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Mastermind = require("./game.js");
 const game = new Mastermind();
 
 function intro() {
+  console.clear()
   console.log(
     "Welcome to MasterMind, you have 10 guesses to make sure you don't blow up! \n"
   );

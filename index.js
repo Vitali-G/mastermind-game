cba937ea9059643e50fec9ffbebdb9de968e05c1
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-03-03 10:20:25 +0000

    adding history to guess function

--- index.js
+++ index.js
@@ -1,45 +1,58 @@
 class Mastermind {
-    constructor({ wordLength }) {
-      this.code = this.generateCode(wordLength || 4);
+  constructor() {
+    this.code = this.generateCode(4);
+    this.guesses = 10;
+    this.guessHistory = [];
+  }
+
+  generateCode(length) {
+    const CHARACTERS = "rgba".split("");
+    let string = "";
+
+    for (let i = 0; i < length; i++) {
+      string += CHARACTERS[Math.floor(Math.random() * CHARACTERS.length)];
     }
-  
-    generateCode(length) {
-      const CHARACTERS = "rgba".split("");
-      let string = "";
-  
-      for (let i = 0; i < length; i++) {
-        string += CHARACTERS[Math.floor(Math.random() * CHARACTERS.length)];
+
+    return string;
+  }
+
+  guess(query) {
+    if (this.code.length != query.length)
+      return "Make sure your guess matches the length of the code";
+
+    if (this.guesses === 0) return "lose";
+    this.guesses--;
+
+    let count = 0;
+    let win = false;
+
+    for (let i = 0; i < this.code.length; i++) {
+      if (this.code[i] === query[i]) {
+        count++;
       }
-  
-      return string;
+
+      if (count === this.code.length) {
+        win = true;
+      }
     }
-  
-    guess(query) {
-      if (this.code.length != query.length)
-        return "Make sure your guess matches the length of the code";
-  
-      let count = 0;
-      let win = false;
-  
-      for (let i = 0; i < this.code.length; i++) {
-        if (this.code[i] === query[i]) {
-          count++;
-        }
-  
-        if (count === this.code.length) {
-          win = true;
-        }
-      }
-  
-      if (win) {
-        return win;
-      } else {
-        return !!count ? count : "no matches";
-      }
+
+    if (win) {
+      return win;
+    } else {
+      this.guessHistory.push({ prev: query, match: count });
+      return count;
     }
   }
-  
-  const game = new Mastermind({ wordLength: 4 });
-  
-  console.log(game.code);
-  console.log(game.guess("rgba"));
+}
+
+const game = new Mastermind();
+
+console.log('correct code: ', game.code);
+
+console.log(game.guess("rgba"));
+
+console.log('history: ', game.guessHistory);
+
+console.log(game.guess("rrbr"));
+
+console.log('history: ', game.guessHistory);
